08fd37ec7cc292713c5c4f8e1882c981bbac910c
DatKami <studiowolf8@gmail.com>
2020-02-10 01:46:24 -0500

    Updates

---
+++ precache-manifest.629904f3cc613b42ec1ee7d152f1e082.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a397785224fca832fad92c4984458b68",
+    "url": "/bartending-interface/index.html"
+  },
+  {
+    "revision": "ccdbd9faf55a3df47cee",
+    "url": "/bartending-interface/static/css/main.f684999c.chunk.css"
+  },
+  {
+    "revision": "c3f9f12f559316a573de",
+    "url": "/bartending-interface/static/js/2.be4a9030.chunk.js"
+  },
+  {
+    "revision": "ac3f8e58b5796b271331f2926a0e8b61",
+    "url": "/bartending-interface/static/js/2.be4a9030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdbd9faf55a3df47cee",
+    "url": "/bartending-interface/static/js/main.10b02db5.chunk.js"
+  },
+  {
+    "revision": "756bc7a0edce78e98672",
+    "url": "/bartending-interface/static/js/runtime-main.5a991387.js"
+  },
+  {
+    "revision": "d06a940ac391a5812fb4c2e96c26d602",
+    "url": "/bartending-interface/static/media/cyberpunkwaifus-webfont.d06a940a.woff"
+  },
+  {
+    "revision": "fc2728cd716c0f863c6e0573feeac45f",
+    "url": "/bartending-interface/static/media/hover.fc2728cd.mp3"
+  },
+  {
+    "revision": "992311011656777ebdbb5af971887016",
+    "url": "/bartending-interface/static/media/tab.99231101.mp3"
+  },
+  {
+    "revision": "a2c73de990eda5bb9af767f0bccb8c8c",
+    "url": "/bartending-interface/static/media/va-11-hall-a-6px-webfont.a2c73de9.woff"
+  }
+]);
